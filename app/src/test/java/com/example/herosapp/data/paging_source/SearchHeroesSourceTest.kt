a10ca175abaab4c486984ebb2144e4d07cdc2660
HIPPODATA\ahs <ahs@hippodata.net>
2022-05-22 20:46:32 +0200

    Adding FakeHeroApi for testing & SearchHeroesSourceTest  ...

---
+++ app/src/test/java/com/example/herosapp/data/paging_source/SearchHeroesSourceTest.kt
@@ -0,0 +1,141 @@
+package com.example.herosapp.data.paging_source
+
+import androidx.paging.PagingSource.LoadParams
+import androidx.paging.PagingSource.LoadResult
+import com.example.herosapp.data.remote.FakeHeroApi
+import com.example.herosapp.data.remote.HeroApi
+import com.example.herosapp.domain.model.Hero
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SearchHeroesSourceTest {
+
+    private lateinit var heroApi: HeroApi
+    private lateinit var heroes: List<Hero>
+
+    @Before
+    fun setup() {
+        heroApi = FakeHeroApi()
+        heroes = listOf(
+            Hero(
+                id = 1,
+                name = "Sasuke",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            ),
+            Hero(
+                id = 2,
+                name = "Naruto",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            ),
+            Hero(
+                id = 3,
+                name = "Sakura",
+                image = "",
+                about = "",
+                rating = 5.0,
+                power = 0,
+                month = "",
+                day = "",
+                family = listOf(),
+                abilities = listOf(),
+                natureTypes = listOf()
+            )
+        )
+    }
+
+    @Test
+    fun `Search api with existing hero name, expect single hero result, assert LoadResult_Page`() =
+        runTest {
+            val heroSource = SearchHeroesSource(heroApi = heroApi, query = "Sasuke")
+            assertEquals<LoadResult<Int, Hero>>(
+                expected = LoadResult.Page(
+                    data = listOf(heroes.first()),
+                    prevKey = null,
+                    nextKey = null
+                ),
+                actual = heroSource.load(
+                    LoadParams.Refresh(
+                        key = null,
+                        loadSize = 3,
+                        placeholdersEnabled = false
+                    )
+                )
+            )
+        }
+
+    @Test
+    fun `Search api with existing hero name, expect multiple hero result, assert LoadResult_Page`() =
+        runTest {
+            val heroSource = SearchHeroesSource(heroApi = heroApi, query = "Sa")
+            assertEquals<LoadResult<Int, Hero>>(
+                expected = LoadResult.Page(
+                    data = listOf(heroes.first(), heroes[2]),
+                    prevKey = null,
+                    nextKey = null
+                ),
+                actual = heroSource.load(
+                    LoadParams.Refresh(
+                        key = null,
+                        loadSize = 3,
+                        placeholdersEnabled = false
+                    )
+                )
+            )
+        }
+
+    @Test
+    fun `Search api with empty hero name, assert empty heroes list and LoadResult_Page`() =
+        runTest {
+            val heroSource = SearchHeroesSource(heroApi = heroApi, query = "")
+            val loadResult = heroSource.load(
+                LoadParams.Refresh(
+                    key = null,
+                    loadSize = 3,
+                    placeholdersEnabled = false
+                )
+            )
+
+            val result = heroApi.searchHeroes("").heroes
+
+            assertTrue { result.isEmpty() }
+            assertTrue { loadResult is LoadResult.Page }
+        }
+
+    @Test
+    fun `Search api with non_existing hero name, assert empty heroes list and LoadResult_Page`() =
+        runTest {
+            val heroSource = SearchHeroesSource(heroApi = heroApi, query = "Unknown")
+            val loadResult = heroSource.load(
+                LoadParams.Refresh(
+                    key = null,
+                    loadSize = 3,
+                    placeholdersEnabled = false
+                )
+            )
+
+            val result = heroApi.searchHeroes("Unknown").heroes
+
+            assertTrue { result.isEmpty() }
+            assertTrue { loadResult is LoadResult.Page }
+        }
+
+}
